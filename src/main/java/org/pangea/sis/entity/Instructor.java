0b1eff6e02dcdf6bfac35664a1041fe50e6da7b9
Kaan <kaan.unsel@hotmail.com>
2025-06-12 12:51:58 +0300

    remove unused imports

--- src/main/java/org/pangea/sis/entity/Instructor.java
+++ src/main/java/org/pangea/sis/entity/Instructor.java
@@ -1,6 +1,5 @@
 package org.pangea.sis.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
